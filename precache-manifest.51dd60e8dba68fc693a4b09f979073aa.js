2e5fd2eaf03a98de91294fadd314173ac55ab6d0
Ritik agarwal <ritik.9869@gmail.com>
2020-10-22 12:17:59 +0530

    Updates

---
+++ precache-manifest.51dd60e8dba68fc693a4b09f979073aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85a6ca0f4ae1ee111c25c1085c2a2bd",
+    "url": "/skillpill/index.html"
+  },
+  {
+    "revision": "43f584f35af7337200cf",
+    "url": "/skillpill/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "0ef0dec9b53a43c647ed",
+    "url": "/skillpill/static/css/main.6b2a96c4.chunk.css"
+  },
+  {
+    "revision": "43f584f35af7337200cf",
+    "url": "/skillpill/static/js/2.64c974e9.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/skillpill/static/js/2.64c974e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef0dec9b53a43c647ed",
+    "url": "/skillpill/static/js/main.117d522c.chunk.js"
+  },
+  {
+    "revision": "d775fc381c11307d43b6",
+    "url": "/skillpill/static/js/runtime-main.9006bbe5.js"
+  }
+]);
